76d6bcb943afff3108eb7a845a3b464b4936ed3d
Youta "_nabbe" T <youta.t@gmail.com>
2018-03-11 23:24:15 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! python
+
+from distutils.core import setup
+
+setup(
+    name='ducktypy',
+    version='0.1.0',
+    description='a library helps ducktyping (almost) anywhere.',
+    author='Youta Takaoka (@_nabbe)',
+    url='https://github.com/nabbe/ducktypy',
+    packages=['ducktypy'],
+)
+
